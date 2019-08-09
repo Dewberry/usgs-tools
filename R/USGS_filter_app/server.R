be2c7a56e900c54d412c9fd368a3bbdec66a1d74
Jason Matney <jamatney@gmail.com>
2019-08-08 21:16:41 -0400

    Update server.R
    add list.of.packages to server.R

--- R/USGS_filter_app/server.R
+++ R/USGS_filter_app/server.R
@@ -1,3 +1,15 @@
+##############################################################################
+list.of.packages <- c("RColorBrewer","dataRetrieval",
+                      "curl","repr","maps","dplyr", "stringr",
+                      "ggplot2","leaflet","leafem","raster",
+                      "raster","shiny","htmlwidgets","devtools",
+                      "shinycustomloader","shinydashboard","shinyjs","DT","DBI",
+                      "spData","sf","shinythemes", "shinyalert", "plotly","tryCatchLog")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+lapply(list.of.packages, require, character.only = TRUE)
+##############################################################################
+
 server <- function(input, output, session) {
   
   ### Pop up disclaimer
