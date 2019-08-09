95f0b4d33fdcbba76ba9eb162fc048814775eb0d
Jason Matney <jamatney@gmail.com>
2019-08-09 09:01:35 -0400

    add shinycssloaders package
    include shiny css loaders

--- R/USGS_filter_app/server.R
+++ R/USGS_filter_app/server.R
@@ -3,7 +3,7 @@ list.of.packages <- c("RColorBrewer","dataRetrieval",
                       "curl","repr","maps","dplyr", "stringr",
                       "ggplot2","leaflet","leafem","raster",
                       "raster","shiny","htmlwidgets","devtools",
-                      "shinycustomloader","shinydashboard","shinyjs","DT","DBI",
+                      "shinycustomloader","shinydashboard","shinycssloaders","shinyjs","DT","DBI",
                       "spData","sf","shinythemes", "shinyalert", "plotly","tryCatchLog")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
