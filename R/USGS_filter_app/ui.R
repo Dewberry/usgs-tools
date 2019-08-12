b635d85ee3e02919473cc1ae6f8de886c7a6cba6
Jason Matney <jamatney@gmail.com>
2019-08-12 08:59:42 -0400

    Fixed crash on submit
    Issue was with data.table package.

--- R/USGS_filter_app/ui.R
+++ R/USGS_filter_app/ui.R
@@ -1,9 +1,9 @@
 ##############################################################################
 list.of.packages <- c("RColorBrewer","dataRetrieval",
-                      "curl","repr","maps","dplyr", "stringr",
+                      "curl","shinycssloaders","skimr","repr","maps","dplyr", "stringr",
                       "ggplot2","leaflet","leafem","raster",
-                      "raster","skimr","shiny","htmlwidgets","devtools",
-                      "shinycustomloader","shinydashboard","shinycssloaders","shinyjs","DT","DBI",
+                      "raster","shiny","htmlwidgets","devtools", "data.table",
+                      "shinydashboard","shinyjs","DT","DBI",
                       "spData","sf","shinythemes", "shinyalert", "plotly","tryCatchLog")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
